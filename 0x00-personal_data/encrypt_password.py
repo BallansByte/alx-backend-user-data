75ce70975424f1d5d97471f9b6c407146f730406
BallansByte <maballans@gmail.com>
2024-09-13 07:09:24 +0000

    Tasks

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Password encryption 
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Creates hashed password """
+    pwd = password.encode()
+    hashed_pwd = bcrypt.hashpw(pwd, bcrypt.gensalt())
+    return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check if hashed password matches
+        unhashed password
+    """
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
